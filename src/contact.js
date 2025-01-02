84a53ea35ddd0c8700236d6fd2f40eea513e94ad
shannu1995 <sivapurapu95@gmail.com>
2025-01-02 15:33:06 +1100

    contact page more beautiful now.

--- src/contact.js
+++ src/contact.js
@@ -1,27 +1,33 @@
 import maleImage from "./images/avatar_male.png"
+import femaleImage from "./images/avatar_female.png"
+
 export const contact_info = () =>{
     let contact_div = document.getElementById("content");
     contact_div.innerHTML =""
-    
-    //const cardDiv = document.createElement('div');
-    //cardDiv.className = 'card';
-    //cardDiv.style.width = '300px';
 
+    const firstCard = createCard(maleImage, 'John Doe', 'Chief Cook');
+    contact_div.appendChild(firstCard)
+
+    const secondCard = createCard(femaleImage, 'Jane Smith', 'Owner');
+    contact_div.appendChild(secondCard)
+};
+
+function createCard(imageSrc, name, description) {
+    
     let image = document.createElement("img");
-    image.src = maleImage;
+    image.src = imageSrc;
 
     const containerDiv = document.createElement("div");
     containerDiv.className = "container"
 
     const heading = document.createElement('h4');
-    heading.innerHTML = '<b>John Doe</b>';
+    heading.innerHTML = `<b>${name}</b>`;
 
     const paragraph = document.createElement('p');
-    paragraph.textContent = 'Chief Cook';
+    paragraph.textContent = description;
 
+    containerDiv.appendChild(image)
     containerDiv.appendChild(heading);
     containerDiv.appendChild(paragraph);
-    
-    contact_div.appendChild(image);
-    contact_div.appendChild(containerDiv);
+    return containerDiv;
 };
