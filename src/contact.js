d30a4229ef2f49a61e11c1aeb2088254bd7dfdad
shannu1995 <sivapurapu95@gmail.com>
2025-01-02 14:40:03 +1100

    Did a basic contact card.

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,27 @@
+import maleImage from "./images/avatar_male.png"
 export const contact_info = () =>{
     let contact_div = document.getElementById("content");
-    contact_div.innerText = "Sample Concact Info"
+    contact_div.innerHTML =""
+    
+    //const cardDiv = document.createElement('div');
+    //cardDiv.className = 'card';
+    //cardDiv.style.width = '300px';
+
+    let image = document.createElement("img");
+    image.src = maleImage;
+
+    const containerDiv = document.createElement("div");
+    containerDiv.className = "container"
+
+    const heading = document.createElement('h4');
+    heading.innerHTML = '<b>John Doe</b>';
+
+    const paragraph = document.createElement('p');
+    paragraph.textContent = 'Chief Cook';
+
+    containerDiv.appendChild(heading);
+    containerDiv.appendChild(paragraph);
+    
+    contact_div.appendChild(image);
+    contact_div.appendChild(containerDiv);
 };
